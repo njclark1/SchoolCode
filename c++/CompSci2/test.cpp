a95f9e30883f8c66c5444dedbc4759f504b503fb
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-10-27 16:36:51 -0400

    Working on Lab3 for compsci2

--- c++/CompSci2/test.cpp
+++ c++/CompSci2/test.cpp
@@ -1,17 +1,36 @@
 //programmer: Nickj.clark14@gmail.com
-//date: 2021 09 14
+//date: 2021 10 27
 
 //include statements
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <algorithm>
+#include <cstring>
 
 //namespaces
 using namespace std;
 
+int calcNumWords(string);
+
 int main() {
-    cout << "what" << endl;
-    	//system pause and return to close main
+
+    char sentence[] = "";
+    cout << "W" << endl;
+    cin.getline(sentence, 10000000000000);
+
+
+    //cout << "What is your string?";
+    //cin >> sentence;
+   // int sentenceLength = sentence.length();
+    //char *pStr = new char[sentenceLength];
+
+    //cout << *pStr << endl;
+  //  cout << sentenceLength << endl;
+    cout << sentence << endl;
+    cout << strlen(sentence) << endl;;
+
+   	//system pause and return to close main
+    system("Pause");
 	return 0;
 }
