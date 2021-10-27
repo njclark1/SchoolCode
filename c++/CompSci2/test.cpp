67f33ac8ce546a53834930a374f85964f26cdbf5
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-10-27 14:47:53 -0400

    setting up c++ development on mac

---
+++ c++/CompSci2/test.cpp
@@ -0,0 +1,17 @@
+//programmer: Nickj.clark14@gmail.com
+//date: 2021 09 14
+
+//include statements
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+
+//namespaces
+using namespace std;
+
+int main() {
+    cout << "what" << endl;
+    	//system pause and return to close main
+	return 0;
+}
