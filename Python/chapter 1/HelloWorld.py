e85759b561764df5469f008a9de7f0859bf9bd18
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-11-01 19:16:54 -0400

    preparing to use python with Julien

--- Python/chapter 1/HelloWorld.py
+++ Python/chapter 1/HelloWorld.py
@@ -3,7 +3,8 @@ b = 12
 c= a + b
 
 def main():    
-    x = "Hello World"
-    print(x, c)
+    x = "Hello Julien"
+    y = "\nLet's have some fun with python!"
+    print (x, y, "\nThe result of this math is: ", c)
 
 main()
