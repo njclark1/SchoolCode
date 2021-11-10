2f05d23b7028fae3dabbfbaef8c0ddd243dbd5f2
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-11-10 16:27:23 -0500

    working on Lab4

--- c++/CompSci2/Lab4/Lab4.cpp
+++ c++/CompSci2/Lab4/Lab4.cpp
@@ -1,5 +1,5 @@
 //programmer: Nickj.clark14@gmail.com
-//date: 2021 10 28
+//date: 2021 11 10
 
 //include statements
 #include <iostream>
@@ -9,6 +9,7 @@
 #include <cstring>
 #include <fstream>
 
+
 //namespaces
 using namespace std;
 
@@ -17,13 +18,22 @@ using namespace std;
 
 //main
 int main() {
-
     //declare variables
-
-    //logic
+    string userFile;
+    string line;
+    int startingLine;
     
-    //output results to user
-    cout << "Hi" << endl;
+
+    int number_of_lines = 0;
+    ifstream myfile("txt.txt");
+
+    while (getline(myfile, line)) 
+        ++number_of_lines;
+    cout << "Number of lines in text file: " << number_of_lines << endl; 
+
+    startingLine = number_of_lines - 9;
+
+
    	//system pause and return to close main
     system("Pause");
 	return 0;
---
+++ c++/CompSci2/Lab4/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+
+    while (getline(myfile, line)) 
