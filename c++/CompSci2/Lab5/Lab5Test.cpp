05d37fdad3e14d0f3cc74719c8d614a676214c4f
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-11-16 13:22:36 -0500

    Start Lab 5

---
+++ c++/CompSci2/Lab5/Lab5Test.cpp
@@ -0,0 +1,30 @@
+//programmer: Nickj.clark14@gmail.com
+//date: 2021 11 16
+
+//include statements
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <cstring>
+#include <fstream>
+
+//namespaces
+using namespace std;
+
+//prototypes
+
+
+//main
+int main() {
+
+    //declare variables
+
+    //logic
+    
+    //output results to user
+    cout << "Hi" << endl;
+   	//system pause and return to close main
+    system("Pause");
+	return 0;
+}
