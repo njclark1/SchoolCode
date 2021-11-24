54953148d521b057bf5a700a14e383015385e2c4
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-11-24 18:05:18 -0500

    lab5 finished

--- c++/CompSci2/Lab5/Lab5.cpp
+++ c++/CompSci2/Lab5/Lab5.cpp
@@ -1,5 +1,5 @@
 //programmer: Nickj.clark14@gmail.com
-//date: 2021 11 16
+//date: 2021 11 24
 
 //include statements
 #include <iostream>
@@ -14,29 +14,44 @@
 using namespace std;
 
 //prototypes
-
+void reverseStr(string);
 
 //main
 int main() {
 
     //declare variables
     string myString = "";
+
     //logic
+    //get input from user
     cout << "What is your string?: " << endl;
     getline(cin, myString);
+
+    //call function to print reverse string
+    reverseStr(myString);
+
     //output results to user
-
-
     cout << myString;
+
    	//system pause and return to close main
     system("Pause");
 	return 0;
-}
+}//end main
 
+// function definition
 void reverseStr(string myString) {
-    if (myString.size() == 0)
-    {
-        return;
-    }
-    cout << myString[0];
-}
+
+    //size of string
+  size_t numChar = myString.size();
+
+    //print each char
+  if(numChar == 1) {
+    cout << myString << endl;
+  }
+  else {
+    cout << myString[numChar - 1];
+
+    //recursion of function
+    reverseStr(myString.substr(0, numChar - 1));
+  }
+}//end reverseStr
