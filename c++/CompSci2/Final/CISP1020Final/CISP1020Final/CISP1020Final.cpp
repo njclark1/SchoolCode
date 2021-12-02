cee6ed1ea544277e508611e661ebe43f49fd3b70
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-12-02 17:29:33 -0500

    get start on final

--- c++/CompSci2/Final/CISP1020Final/CISP1020Final/CISP1020Final.cpp
+++ c++/CompSci2/Final/CISP1020Final/CISP1020Final/CISP1020Final.cpp
@@ -1,20 +1,48 @@
-// CISP1020Final.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
+//programmer: Nickj.clark14@gmail.com
+//date: 2021 12 02
+
+/*
+For the final assignment, write a program that lets the user convert pounds to kilogramsand kilograms
+to pounds.
+
+Use function prototypes in your coding, and include a menu that will allow the user to
+choose which conversion to perform, input 2 variables, and then output the result of the conversion.
+
+Put the code in the Dropbox by Dec. 7, 2021.
+
+function for menu to call conversion functions
+function to convert lbs to kgs
+function to convert kgs to lbs
+use prototypes for each function
+output the results to user
+*/
+
+//include statements
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <cstring>
+
+//namespaces
+using namespace std;
+
+//prototypes
+//int calcNumWords(const char*);
+//int calcNumWords(const char*);
 
 int main()
 {
+
+    //declare variables
+    int myint = 0;
+
+    //output results to user
     std::cout << "Hello World!\n";
+
+    //system pause and return to close main
+    system("Pause");
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
