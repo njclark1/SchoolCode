9c3130e8f73ba09a561b7676d209329574aaa7e9
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-12-08 19:31:20 -0500

    working on writing student answers to array

--- c#/capstoneProjectPrototype/consoleVersionV1/consoleVersionV1/Program.cs
+++ c#/capstoneProjectPrototype/consoleVersionV1/consoleVersionV1/Program.cs
@@ -16,10 +16,14 @@ class Program
         //introduce program to user
         Console.WriteLine("Hello, this program will grade your exam");
 
-        //call functiton to read student answer file
-        readStudentFile();        
-        
-        //call functiton to read correct answer file
+        //call function to read student answer file
+        readStudentFile();
+
+        //call function to write student answers to array 
+        Console.WriteLine("\nwriting student answers to array\n");
+        writeStudentArray();
+
+        //call function to read correct answer file
         readCorrectFile();
 
     }//end main function
@@ -33,7 +37,7 @@ class Program
         //read file
         if (File.Exists(studentPath))
         {
-
+            //log if file found
             Console.WriteLine("\nFound student file\n");
             int counter = 0;
 
@@ -55,7 +59,7 @@ class Program
         //read file
         if (File.Exists(correctPath))
         {
-
+            //log if file found
             Console.WriteLine("\nFound correct answer file\n");
             int counter = 0;
 
@@ -67,4 +71,22 @@ class Program
             }
         }
     }//end readcorrectfile
+
+    //function to write student answers to array
+    public static Array[] writeStudentArray()
+    {
+        //variables
+        Array[] studentArray;
+        studentArray = new Array[20];
+
+        //loop to write array
+        for (int i = 0; i < studentArray.Length; i++)
+        {
+            Console.WriteLine("test");
+        }
+        //return array
+        return studentArray;
+    }//end function to write student answers to array
+
 }//end program
+
