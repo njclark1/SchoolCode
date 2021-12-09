05ece5e4a9d0a43167c092005c3852cd450cd6ab
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-12-08 21:13:43 -0500

    both files writing to arrays

--- c#/capstoneProjectPrototype/consoleVersionV1/consoleVersionV1/Program.cs
+++ c#/capstoneProjectPrototype/consoleVersionV1/consoleVersionV1/Program.cs
@@ -13,24 +13,39 @@ class Program
     //main function
     static void Main(string[] args)
     {
+        //variables
+        int numAnswersIncorrect = 0;
+        //student answer array
+        string[] studentArray;
+        studentArray = new string[20];
+
+        //correct answer array
+        string[] correctArray;
+        correctArray = new string[20];
+
         //introduce program to user
         Console.WriteLine("Hello, this program will grade your exam");
 
         //call function to read student answer file
-        readStudentFile();
+        readStudentFile(studentArray);
+        testPrintArray(studentArray);
 
-        //call function to write student answers to array 
-        Console.WriteLine("\nwriting student answers to array\n");
-        writeStudentArray();
 
         //call function to read correct answer file
-        readCorrectFile();
+        readCorrectFile(correctArray);
+        testPrintArray(correctArray);
+
+        //call function to calculate the number of incorrect answers
+        calcNumIncorrectAnswers(numAnswersIncorrect, studentArray, correctArray);
 
     }//end main function
 
     //function to parse student answer file
-    static void readStudentFile()
+    static void readStudentFile(string[] studentArray)
     {
+        //update user
+        Console.WriteLine("reading student file to array");
+
         //variables
         string studentPath = "studentAnswers.txt";
 
@@ -44,14 +59,16 @@ class Program
             // Read the file and display it line by line.  
             foreach (string line in File.ReadLines(studentPath))
             {
-                Console.WriteLine(line);
+                studentArray[counter] = line;
                 counter++;
             }
         }
     }//end readstudentfile
 
     //function to parse correct answer file
-    static void readCorrectFile() { 
+    static void readCorrectFile(string[] correctArray) {
+        //update user
+        Console.WriteLine("reading correct answer file to array");
 
         //variables
         string correctPath = "correctAnswers.txt";
@@ -66,27 +83,29 @@ class Program
             // Read the file and display it line by line.  
             foreach (string line in File.ReadLines(correctPath))
             {
-                Console.WriteLine(line);
+                correctArray[counter] = line;
                 counter++;
             }
         }
     }//end readcorrectfile
 
-    //function to write student answers to array
-    public static Array[] writeStudentArray()
+    //function to print chosen array to console
+    static void testPrintArray(string[] printArray)
     {
-        //variables
-        Array[] studentArray;
-        studentArray = new Array[20];
-
-        //loop to write array
-        for (int i = 0; i < studentArray.Length; i++)
+        Console.WriteLine("\nprinting array chosen to test\n");
+        int counter = 0;
+        foreach (string line in printArray)
         {
-            Console.WriteLine("test");
+            Console.WriteLine(line);
+            counter++;
         }
-        //return array
-        return studentArray;
-    }//end function to write student answers to array
+    }//end testprint function
 
-}//end program
+    //function to calculate the number of incorrect answers
+    static void calcNumIncorrectAnswers(int numIncorrect, string[] studentArray, string[] correctArray)
+    {
+        Console.WriteLine("calculating number of incorrect answers");
+    }//end calcincorrect function
+
+    }//end program
 
